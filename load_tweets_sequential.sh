9f9dd6e69b279751629d787bd8933fe12e56518d
Valentina Watson <123014799+ValentinaWatson@users.noreply.github.com>
2024-04-13 19:20:07 -0700

    hw

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 files=$(find data/*)
 
 echo '================================================================================'
 echo 'load denormalized'
 echo '================================================================================'
-time for file in $files; do
-    time unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:6666/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+
+for file in $files; do
+     time unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:6666/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
+
 
 echo '================================================================================'
 echo 'load pg_normalized'
